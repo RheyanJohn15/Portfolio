e4970c5b5462834a7e24ffcfde0c08b9d0938fb4
RheyanCutie12345 <118708912+RheyanCutie12345@users.noreply.github.com>
2023-12-03 10:29:16 +0800

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,124 @@
+import Pic from "./assets/images/myPic.jpg";
+import Port1 from "./assets/images/port1.png";
+const logotext = "RHEYAN";
+const meta = {
+    title: "Rheyan John Blanco",
+    description: "I’m Rheyan John Blanco data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Rheyan John Blanco",
+    animated: {
+        first: "I love coding",
+        second: "I excel in back end",
+        third: "I can do full stack",
+    },
+    description: "Striving to weave digital dreams into reality as an aspiring web developer, poised for success.",
+    your_img_url: Pic,
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "An aspiring web developer, I am fervently eager to deepen my knowledge within this dynamic industry. Self-studying programming for the past three years has fueled my passion for coding. My goal is to thrive and succeed in this career path.",
+};
+const worktimeline = [{
+        jobtitle: "Computer System Servicing NC2",
+        where: "2020",
+        date: "2025",
+    },
+    {
+        jobtitle: "Google Analytics for Begginers",
+        where: "2023",
+        date: "2026",
+    },
+    {
+        jobtitle: "Google Analytics for Power Users",
+        where: "2023",
+        date: "2026",
+    },
+    {
+        jobtitle: "Google Analytics Advance",
+        where: "2023",
+        date: "2026",
+    },
+];
+
+const skills = [
+    {
+        name: "PHP",
+        value: 90,
+    },
+    {
+        name: "Laravel",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "Django",
+        value: 40,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 40,
+    },
+    {
+        name: "Java",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Web Developer",
+        description: "I can use various frameworks such as laravel and django i ca",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+   
+];
+
+const dataportfolio = [{
+        img: Port1,
+        description: "A web base faculty performance evaluation system for notre dame",
+        link: "https://perfometrics-notre-dame.com/",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rheyanjohn.blanco@chmsc.edu.ph",
+    YOUR_FONE: "09917584477",
+    description: "I am passionate about creating innovative web solutions that merge functionality with elegant design. With a strong foundation in programming and an eye for detail, I am dedicated to crafting engaging user experiences. Let's collaborate and bring your ideas to life!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_hjbxnsm",
+    YOUR_TEMPLATE_ID: "template_vzli0kd",
+    YOUR_USER_ID: "o7lyZgjPmms0jKjDx",
+};
+
+const socialprofils = {
+    github: "https://github.com/RheyanJohn15",
+    facebook: "https://www.facebook.com/rheyanjohn.blanco/",
+    linkedin: "https://www.linkedin.com/in/rheyanjohn-blanco-256090287/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
